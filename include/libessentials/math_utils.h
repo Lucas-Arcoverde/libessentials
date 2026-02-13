ac8737aefcfd264366ce3dba2c6b77d0469b5f50
Lucas Arcoverde <arcoverdemlucas@gmail.com>
2026-02-13 20:04:28 -0300

    Create basic math operations functions.

---
+++ include/libessentials/math_utils.h
@@ -0,0 +1,16 @@
+/*
+ * libessentials
+ * Copyright (c) 2026 Lucas Arcoverde de Melo
+ * Licensed under the MIT License
+ */
+
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+double ess_add(double first_number, double second_number);
+double ess_sub(double first_number, double second_number);
+double ess_mul(double first_number, double second_number);
+double ess_div(double first_number, double second_number);
+
+#endif //MATH_UTILS_H
+
