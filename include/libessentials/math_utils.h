4dddb4b48b1d2faf722dc93cbfda25211146f92f
Lucas Arcoverde <arcoverdemlucas@gmail.com>
2026-02-13 20:12:41 -0300

    Create basic math operations functions for integers.

--- include/libessentials/math_utils.h
+++ include/libessentials/math_utils.h
@@ -12,5 +12,10 @@ double ess_sub(double first_number, double second_number);
 double ess_mul(double first_number, double second_number);
 double ess_div(double first_number, double second_number);
 
+int ess_sum_int(int first_number, int second_number);
+int ess_sub_int(int first_number, int second_number);
+int ess_mul_int(int first_number, int second_number);
+int ess_div_int(int first_number, int second_number);
+
 #endif //MATH_UTILS_H
 
